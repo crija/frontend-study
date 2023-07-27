86d0d38f88ce795de26f578e268fc993690bc300
crija <crijacarla@gmail.com>
2023-07-27 19:08:24 -0300

    Add variables

---
+++ javascript/variaveis.js
@@ -0,0 +1,29 @@
+// variáveis
+
+let idade = 26
+
+console.log(idade) // criando a primeira variável
+
+// Tipos de variáveis
+
+idade = 39
+let numero = 30
+let altura = 1.77
+let nome = 'walisson'
+let estudando = true
+
+console.log(idade, typeof idade) //typeof printa o tipo de dado
+console.log(numero)
+console.log(altura)
+console.log(nome)
+console.log(estudando)
+
+let outra; // variavel não definida
+
+console.log(outra)
+
+let nula = null // variável nula
+
+console.log(nula)
+
+const constante = 100 // variável constante, não pode ser modificada

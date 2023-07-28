0ed093078351de221068c4a22eb5c4760f3fbd43
crija <crijacarla@gmail.com>
2023-07-28 19:33:53 -0300

    Add logical conjunctions

--- javascript/operadores_bool.js
+++ javascript/operadores_bool.js
@@ -7,3 +7,26 @@ console.log(numero >= 12)
 console.log(numero == '10') // compara o conteúdo
 console.log(numero === 10) // compara o tipo de dado
 console.log(numero !== '10') // dica: usar sempre o === ou !== quando for comparar
+
+// Conjunções Lógicas
+
+// AND => &&
+
+let idade = 18
+let tenhoCNH = true
+
+const possoDirigir = idade >= 18 && tenhoCNH === true
+
+console.log('Posso dirigir?', possoDirigir)
+
+// OR => ||
+
+idade = 40
+
+const votoFacultativo = idade < 18 || idade >= 70
+
+// NOT => !
+
+const estouFeliz = false
+
+console.log(!estouFeliz)

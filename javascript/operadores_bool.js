71f1fe82bc41d06a47ef059f4c8f3cd40c715c20
crija <crijacarla@gmail.com>
2023-07-28 12:10:56 -0300

    Add boolean opeeators

---
+++ javascript/operadores_bool.js
@@ -0,0 +1,9 @@
+const numero = 10;
+
+console.log(numero > 20)
+console.log(numero <= 23)
+console.log(numero >= 12)
+
+console.log(numero == '10') // compara o conteúdo
+console.log(numero === 10) // compara o tipo de dado
+console.log(numero !== '10') // dica: usar sempre o === ou !== quando for comparar
